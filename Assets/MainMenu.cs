8ce014e93550fa93e344f56a556fcd47f851d946
Adele Gikonyo <53218392+adelewg@users.noreply.github.com>
2022-08-28 11:08:49 +0100

    Added the script to play and quit game
    Added the script to play and quit game

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,10 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
    
 
     public void QuitGame()
     {
+        Debug.Log("We have quit game");
         Application.Quit();
     }
     
