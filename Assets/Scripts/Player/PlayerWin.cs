4d4e3a330029fd9ab9f11985b0a192a8bcc39d0f
Robert Allsop <robertallsop51@hotmail.co.uk>
2022-09-01 14:57:22 +0100

    Base for winning and losing

---
+++ Assets/Scripts/Player/PlayerWin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWin : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        EventManager.OnPlayerWin += Win;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnPlayerWin -= Win;
+    }
+
+    private void Win()
+    {
+        // code to display you win.
+    }
+}
