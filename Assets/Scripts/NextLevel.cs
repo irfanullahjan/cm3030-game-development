7c92a5e12b5793573f988a72eb85781853e0cc11
Irfanullah Jan <irfanullah.jan@outlook.com>
2022-09-01 15:04:48 +0500

    level switch when player crosses finish line, camera stops following when player reached finish line

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour
+{
+    private GameObject player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if player crosses the finish line, load next level
+        if (player.transform.position.x > transform.position.x)
+        {
+            // load next level
+            Application.LoadLevel(Application.loadedLevel + 1);
+
+            // add player to next level
+            GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(0, 0, 0);
+        }
+    }
+}
